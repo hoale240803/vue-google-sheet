153850c9efc787e0d74a197997a91877c8845575
HoaLe <hoale240803@gmail.com>
2025-06-19 15:15:52 +0700

    remove sh file

--- vercel-ignore-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ignore build if only .github/workflows files changed
-if git diff --name-only $VERCEL_GIT_COMMIT_REF $VERCEL_GIT_PREVIOUS_SHA | grep -vE '^\.github/workflows/' | grep -q .; then
-    exit 1
-else
-    echo "Only workflow files changed, skipping build."
-    exit 0
-fi
